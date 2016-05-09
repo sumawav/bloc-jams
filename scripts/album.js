fe416677ac055e7377ae3c0f0623a53f2c7a0ee6
Souma Mondal <mondal.souma@gmail.com>
2016-05-09 10:01:04 -0500

    adds play arrow on mouseover

--- scripts/album.js
+++ scripts/album.js
@@ -33,7 +33,7 @@ var albumMarconi = {
 var createSongRow = function(songNumber, songName, songLength) {
     var template =
        '<tr class="album-view-song-item">'
-     + '  <td class="song-item-number">' + songNumber + '</td>'
+     + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
      + '  <td class="song-item-title">' + songName + '</td>'
      + '  <td class="song-item-duration">' + songLength + '</td>'
      + '</tr>'
@@ -65,6 +65,29 @@ var setCurrentAlbum = function(album) {
   }
 };
 
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+
+// Album button templates
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
 window.onload = function() {
   setCurrentAlbum(albumPicasso);
+
+  songListContainer.addEventListener('mouseover', function(event){
+    if (event.target.parentElement.className === 'album-view-song-item') {
+      event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+    }
+  });
+
+  for (var i = 0; i < songRows.length; ++i) {
+    songRows[i].addEventListener('mouseleave', function(event) {
+      //
+      // Okay, what exactly is "this" in this situation?
+      //
+      this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+    });
+  }
+
 };
